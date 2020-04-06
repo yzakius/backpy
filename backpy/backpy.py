0a69b386ca503a11576a818a85bdb6dd284e5d62
yzakius <isaacmob@gmail.com>
2020-04-05 22:36:51 -0300

    Changed project strutcture

---
+++ backpy/backpy.py
@@ -0,0 +1,16 @@
+import os
+
+dest = '/run/media/yzakius/backup/backup/yzakius/'
+source = '/home/yzakius/'
+
+
+def backup(source, dest):
+    if os.path.exists(dest):
+        return os.system(
+            f'sudo rsync -azvP --delete {source} {dest}'
+            )
+    else:
+        return "Error: Where is my HD? =("
+
+
+backup(source, dest)
